6c31c24b2954c404c162acbd7d292ff03fd65967
nnnhuan251-hcmus <nnnhuan251@gmail.com>
2026-06-12 20:39:28 +0700

    Create merge_coco_datasets.py

---
+++ scripts/data/merge_coco_datasets.py
@@ -0,0 +1,9 @@
+"""
+Input:
+- TACO annotations_7class.json
+- Roboflow train_annotations.json / val_annotations.json / test_annotations.json hoặc một file COCO ngoài
+
+Output:
+- mixed_coco_7class/annotations_7class.json
+- merge_report.json
+"""
